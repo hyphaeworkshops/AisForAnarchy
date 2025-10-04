a5f4b3b7b63c5a979e49caa5301ba952b7c1329a
ConorHoehn <conorhoehn@gmail.com>
2025-10-03 20:44:50 -0500

    Somehow missed committing two files with escapes

--- src/app/author/page.tsx
+++ src/app/author/page.tsx
@@ -2,7 +2,7 @@ export default function Author() {
   return (
     <div className="author">
       <p>Conor Hoehn put this website and its content on the internet because it seemed like a good idea at the time.</p>
-      <p>There's a new generation of people being raised every moment.  Those people will contribute to how our society is shaped and how we interact with each other.  It seems prudent to try to teach them both of their own autonomy and the beneficial ways in which they can exercise it.</p>
+      <p>There&apos;s a new generation of people being raised every moment.  Those people will contribute to how our society is shaped and how we interact with each other.  It seems prudent to try to teach them both of their own autonomy and the beneficial ways in which they can exercise it.</p>
     </div>
   );
 }
